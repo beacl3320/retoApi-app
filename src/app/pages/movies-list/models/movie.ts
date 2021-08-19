1849874f1ddd226bba71906ca53f1c1c3091694a
Beatriz Carpio$ git config --global user.name Beatriz <beatriz.carpio@bootcamp-upgrade.com>
2021-08-19 20:00:35 +0200

    FEAT: add services and funcionallity to movie list

---
+++ src/app/pages/movies-list/models/movie.ts
@@ -0,0 +1,15 @@
+export interface Movie {
+  id: number,
+  title: string,
+  original_title: string,
+  poster_path: string,
+  overview: string,
+  genre_ids: number,
+  vote_average: number,
+  release_date: number,
+  vote_acount: number,
+};
+
+/* export interface MovieResponse {
+
+} */
