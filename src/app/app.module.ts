e42e9fd88619181d18d81e273ee94dc8ad4c1527
Beatriz Carpio$ git config --global user.name Beatriz <beatriz.carpio@bootcamp-upgrade.com>
2021-10-05 14:30:51 +0200

    DELETE: components main and movies-list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-/* import { MainComponent } from './components/main/main.component'; */
 import { FooterComponent } from './components/footer/footer.component';
-/* import { MoviesListComponent } from './pages/movies-list/movies-list.component'; */
 import { MovieListComponent } from './pages/movie-list/movie-list.component';
 import { MovieItemComponent } from './pages/movie-item/movie-item.component';
 import { PopularListComponent } from './pages/popular-list/popular-list.component';
@@ -22,16 +19,12 @@ import { FilterPipePipe } from './pipe/filter-pipe.pipe';
   declarations: [
     AppComponent,
     HeaderComponent,
-    /* MainComponent, */
     FooterComponent,
-    /* MoviesListComponent, */
     MovieListComponent,
     MovieItemComponent,
     PopularItemComponent,
     PopularListComponent,
     FilterPipePipe,
-
-
   ],
 
   imports: [
